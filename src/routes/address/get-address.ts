7ffca62495a620693aadcbecc8d7791fb90add74
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-08-12 10:05:19 -0300

    fix: fix function name

--- src/routes/address/get-address.ts
+++ src/routes/address/get-address.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection";
 import { schema } from "../../db/schema";
 
-export const getDepartment: FastifyPluginCallbackZod = (app) => {
+export const getAddress: FastifyPluginCallbackZod = (app) => {
   app.get("/addresses", async () => {
     return db.select().from(schema.addresses)
   })
