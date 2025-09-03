ca670024c6ac18927bc43fbd3d03235ab85e647b
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-09-03 11:16:24 -0300

    refac: romvoe unnecessary org id on body

--- src/routes/gift/create-gift.ts
+++ src/routes/gift/create-gift.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "../../middlewares/auth";
 import z from "zod";
 import { db } from "../../db/connection";
 import { schema } from "../../db/schema";
-import { and, eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { BadRequestError } from "../../helpers/api-error";
 import { authorize } from "../../middlewares/authorize";
 
@@ -13,32 +13,31 @@ export const createGift: FastifyPluginCallbackZod = (app) => {
       body: z.object({
         employee_id: z.uuid(),
         gift_type: z.string(),
-        organization_id: z.uuid(),
         send_date: z.coerce.date(),
-        delivery_date: z.coerce.date()
+        delivery_date: z.coerce.date().nullable().optional()
       })
     },
-    preHandler: [authMiddleware, authorize(['admin', 'manager'])]
+    preHandler: [authMiddleware, authorize(["admin", "manager"], false)]
   },
   async (request, response) => {
-    const { delivery_date, employee_id, gift_type, send_date, organization_id } = request.body;
-
-    const orgExists = await db.query.organizations.findFirst({
-      where: eq(schema.organizations.id, organization_id)
-    });
-
-    if (!orgExists) {
-      throw new BadRequestError("Organization does not exist");
-    }
+    const { delivery_date, employee_id, gift_type, send_date } = request.body;
 
     if (!request.user.id) {
       throw new BadRequestError("User ID is missing");
     }
 
+    const employee = await db.query.employees.findFirst({
+      where: eq(schema.employees.id, employee_id)
+    });
+
+    if (!employee) {
+      throw new BadRequestError("Employee does not exist");
+    }
+
     const userBelongsToOrg = await db.query.usersOrganizations.findFirst({
       where: and(
         eq(schema.usersOrganizations.userId, request.user.id),
-        eq(schema.usersOrganizations.organizationId, organization_id)
+        eq(schema.usersOrganizations.organizationId, employee.organization_id)
       )
     });
 
@@ -46,25 +45,16 @@ export const createGift: FastifyPluginCallbackZod = (app) => {
       throw new BadRequestError("You do not belong to this organization");
     }
 
-    const employeeExists = await db.query.employees.findFirst({
-      where: and(
-        eq(schema.employees.id, employee_id),
-        eq(schema.employees.organization_id, organization_id)
-      )
-    });
-
-    if (!employeeExists) {
-      throw new BadRequestError("Employee does not exist in your organization");
-    }
-
     const [newGift] = await db.insert(schema.gifts).values({
       employee_id,
-      organization_id,
+      organization_id: employee.organization_id,
       gift_type,
       send_date: send_date.toISOString().split("T")[0],
-      delivery_date: delivery_date ? delivery_date.toISOString().split("T")[0] : null,
+      delivery_date: delivery_date
+        ? delivery_date.toISOString().split("T")[0]
+        : null,
     }).returning();
 
     return response.status(201).send(newGift);
-  })
-}
+  });
+};
